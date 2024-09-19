2bd2e75b954136faa3018578c3ead4e4079b05dd
Mehedihasan-99 <1mehedisoykot@gmail.com>
2024-09-19 08:35:06 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -24,16 +24,28 @@
 document.getElementById('btn-login')
     .addEventListener('click', function(event){
     // step:2 prevent Default behavior(prevelent reloading browser)
-    event.preventDefault();
-    console.log('btn clicked');
+    event.preventDefault(); 
+    // console.log('btn clicked');
 
-    // step:3 get the phone number 
+    // step:3 get the phone number & pin number
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(phoneNumber, pinNumber);
+    // console.log(phoneNumber, pinNumber);
+
+    // step:4 validate phone & pin
+    // this is temporary
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href='/home.html';
+
+        // step:5 allow user to use the website 
+    }
+    else{
+        alert('Wrong phone number or pin')
+    }
 })
 
 
 
-// step:1
+
 // step:1
